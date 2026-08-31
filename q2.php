5f860e2e5aba640382dd9bb64d1991db4d65a594
Oscar Hugo Paz <oscarhugopazmariscal@gmail.com>
2026-08-31 16:41:13 -0500

    Release 0.1.5

--- q2.php
+++ q2.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Q2
  * Plugin URI:        https://github.com/plakio/q2
  * Description:       A modern, self-hosted collaborative workspace for WordPress.
- * Version:           0.1.4
+ * Version:           0.1.5
  * Requires at least: 7.1
  * Requires PHP:      8.1
  * Author:            Q2 contributors
@@ -17,7 +17,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'Q2_VERSION', '0.1.4' );
+define( 'Q2_VERSION', '0.1.5' );
 define( 'Q2_PATH', plugin_dir_path( __FILE__ ) );
 define( 'Q2_URL', plugin_dir_url( __FILE__ ) );
 
